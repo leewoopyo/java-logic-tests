0d66a49e6cb88b572923e050adebe3424051d539
woopii <lozenkrantz@gmail.com>
2024-02-19 23:49:05 +0900

    [CHG] DataBase Connection url, Database Diretory

--- database_connection/DatabaseConnection.java
+++ database_connection/DatabaseConnection.java
@@ -2,18 +2,21 @@ package database_connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.util.logging.Logger;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 public class DatabaseConnection {
-    
     public static void main(String[] args) {
+
+        Logger log = Logger.getLogger(DatabaseConnection.class.getName());
         
-        //h2 jdbc 드라이버가 있는지 확인
+        // h2 jdbc 드라이버가 있는지 확인
+        // 추후  mariadb나 oracle db 도 테스트해봐야 할듯
         try {
             Class.forName("org.h2.Driver");
 
@@ -31,7 +34,7 @@ public class DatabaseConnection {
 
             DatabaseConnectInfo databaseConnectInfoH2dbFile = DatabaseConnectInfo.builder()
                 .databaseType("H2DB")     // ORACLE, MARIADB H2DB 
-                .url("jdbc:h2:file:./database_connection/test;MODE=MySQL;")
+                .url("jdbc:h2:file:./.db/test;MODE=MySQL;")
                 .schema("")
                 .userName("sa")
                 .password("")
@@ -41,12 +44,16 @@ public class DatabaseConnection {
             // 정보를 바탕으로 Connection 연결
             Connection connection = DriverManager.getConnection(databaseConnectInfoH2dbFile.getUrl(), databaseConnectInfoH2dbFile.getUserName(), databaseConnectInfoH2dbFile.getPassword());
 
-            Statement statement = connection.createStatement();
-
+            if (connection.isValid(0)) {
+                log.info("DataBase Is Valid");
+            } else {
+                log.info("DataBase Is Not Valid");
+            }
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
-        }
+    }
 
     // Inner Class
 
